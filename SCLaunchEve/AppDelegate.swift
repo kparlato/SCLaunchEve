154e66909650d41d8a3846274175fd90d93b9011
Karen Parlato <lx9fly5@gmail.com>
2019-07-23 09:56:41 -0400

    Launcher command appears to run but does nothing.

--- SCLaunchEve/AppDelegate.swift
+++ SCLaunchEve/AppDelegate.swift
@@ -13,15 +13,71 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
 
+    var item:NSStatusItem? = nil
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        window.close()
+        
+        // Set up menu icon and menu options
+        item = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        item?.button?.title = "Launch Eve"
+        item?.button?.image = NSImage(named: "sclogo")
+        item?.button?.action = #selector(AppDelegate.launchEve)
+        
+        let menu = NSMenu()
+        menu.addItem(NSMenuItem(title: "Launch All", action: #selector(AppDelegate.launchEve), keyEquivalent: ""))
+        menu.addItem(NSMenuItem(title: "Launcher Only", action: #selector(AppDelegate.terminalLauncher), keyEquivalent: ""))
+        menu.addItem(NSMenuItem(title: "Web Tools Only", action: #selector(AppDelegate.launchWebsites), keyEquivalent: ""))
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(AppDelegate.quit), keyEquivalent: ""))
+        item?.menu = menu
     }
 
+    @objc func launchEve() {
+        // "Launch All"
+        terminalLauncher()
+        launchWebsites()
+    }
+    
+    @objc func launchWebsites() {
+        // "Web Tools Only"
+        launchInDefaultBrowser(url: URL(string: "https://evescoutrescue.com/copilot/index.php"))
+        launchInDefaultBrowser(url: URL(string: "https://tripwire.eve-apps.com"))
+    }
+    
+    
+    @objc func terminalLauncher() {
+        // "Launcher Only"
+        // sends the terminal command "eveonline" to launch the Wine version of the launcher
+        
+        let task = Process()
+        task.executableURL = URL(fileURLWithPath: "/usr/local/bin/eveonline")
+        
+        
+        // Launch the task
+        do {
+            try task.run()
+            print ("Task running")
+        } catch {
+            print ("Error running task: \(error)")
+        }
+        
+        task.waitUntilExit()
+        
+    }
+    
+    
+    func launchInDefaultBrowser(url:URL?){
+        if url != nil {
+            NSWorkspace.shared.open(url!)
+        }
+    }
+    
+    @objc func quit() {
+        NSApplication.shared.terminate(self)
+    }
+    
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
 }
 
